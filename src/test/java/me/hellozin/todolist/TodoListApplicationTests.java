cddcbae103a7b3ff43b2846b58ebcc5342c04155
hellozin <paul26375@gmail.com>
2019-05-14 19:00:57 +0900

    test: TODO 제목 및 내용 수정 테스트 작성

--- src/test/java/me/hellozin/todolist/TodoListApplicationTests.java
+++ src/test/java/me/hellozin/todolist/TodoListApplicationTests.java
@@ -1,27 +1,23 @@
 package me.hellozin.todolist;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
-
 import java.util.List;
+import java.util.Optional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,6 +30,20 @@ public class TodoListApplicationTests {
     @Autowired
     MockMvc mockMvc;
 
+    private Todo todoForTest;
+
+    @Before
+    public void cleanRepository() {
+        todoRepository.deleteAll();
+
+        todoForTest = new Todo();
+        todoForTest.setAuthor("hellozin");
+        todoForTest.setTitle("New TODO");
+        todoForTest.setContent("testing");
+
+        todoRepository.save(todoForTest);
+    }
+
     @Test
     public void login() throws Exception {
         mockMvc.perform(post("/login")
@@ -45,41 +55,54 @@ public class TodoListApplicationTests {
 
     @Test
     public void showTodoList() throws Exception {
-        Todo todo = new Todo();
-        todo.setAuthor("hellozin");
-        todo.setTitle("New TODO");
-        todo.setContent("testing");
-
-        todoRepository.save(todo);
-
         MvcResult mvcResult = mockMvc.perform(get("/list")
-                .cookie(new Cookie("author", "hellozin"))
+                .cookie(new Cookie("author", todoForTest.getAuthor()))
         )
                 .andExpect(status().isOk())
                 .andReturn();
 
         List<Todo> todoList = (List) mvcResult.getModelAndView().getModel().get("todoList");
-        assert todoList.get(0).getContent().equals("testing");
+        assert todoList.get(0).getContent().equals(todoForTest.getContent());
     }
 
     @Test
     public void createTodoTest() throws Exception {
-        Todo todo = new Todo();
-        todo.setAuthor("hellozin");
-        todo.setTitle("New TODO");
-        todo.setContent("testing");
-
         mockMvc.perform(post("/todo")
-                .cookie(new Cookie("author", todo.getAuthor()))
-                .param("title", todo.getTitle())
-                .param("content", todo.getContent())
+                .cookie(new Cookie("author", "hellozin"))
+                .param("title", "Another TODO")
+                .param("content", "create testing")
         )
                 .andDo(print())
                 .andExpect(status().is3xxRedirection());
 
-        List<Todo> allByAuthor = todoRepository.findAllByAuthor(todo.getAuthor());
-        assert allByAuthor.get(0).getContent().equals("testing");
+        List<Todo> allByAuthor = todoRepository.findAllByAuthor("hellozin");
+        assert allByAuthor.get(1).getContent().equals("create testing");
 
     }
 
+    @Test
+    public void deleteTodoTest() throws Exception {
+        Todo todoForDelete = todoRepository.findAllByAuthor(todoForTest.getAuthor()).get(0);
+
+        mockMvc.perform(delete("/todo")
+                .param("id", String.valueOf(todoForDelete.getId()))
+                .cookie(new Cookie("author", todoForTest.getAuthor()))
+        )
+                .andExpect(status().is3xxRedirection());
+
+        assert todoRepository.count() == 0;
+    }
+
+    @Test
+    public void updateTodoTest() throws Exception {
+        mockMvc.perform(put("/todo")
+                .param("id", String.valueOf(todoForTest.getId()))
+                .param("title", "Changed Title")
+                .param("content", "Changed Content")
+        )
+                .andExpect(status().is3xxRedirection());
+
+        Optional<Todo> changedTodo = todoRepository.findById(todoForTest.getId());
+        assert changedTodo.orElse(todoForTest).getTitle().equals("Changed Title");
+    }
 }
